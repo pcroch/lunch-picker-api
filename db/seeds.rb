cf5eb3e9b771f89618011a214581ca23895a14bb
pierre crochet <pierrecrochet03@gmail.com>
2021-02-25 11:57:34 +0000

    integration testing done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@ Preference.create(user_id: 1, name: 'Pierre', taste: %w[Italian Lebanese Japanes
 Preference.create(user_id: 2, name: 'Gilles', taste: %w[Italian Lebanese Japanese Belgian])
 Preference.create(user_id: 2, name: 'Vince', taste: %w[Italian Japanese Lebanese])
 Preference.create(user_id: 4, name: 'Sam', taste: %w[Belgian])
+
+Lunch.create(id: 1000, localisation: "Arlon", distance: 1000, price: [1, 4], user_id: '1')
+Lunch.create(id: 1001, localisation: "Saint Gilles", distance: 50, price: [1, 4], user_id: '1')
